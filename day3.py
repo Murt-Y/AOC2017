8cf72c432d53d808867e8db2c8ec52602523b97b
aplainzetakind <thisisnot@valid.org>
2021-02-01 21:45:35 +0300

    Some simplicifications for day 3 part 1.

--- day3.py
+++ day3.py
@@ -1,18 +1,32 @@
+from math import sqrt
+
 inp=266330
-edge=1
-total=edge*edge
-while total<inp:
-    edge=edge+2
-    total=edge*edge
-step=(inp-((edge-2)*(edge-2)))%(edge-1)
-step=abs(step-((edge-1)/2))
-step=step+(edge-1)/2
+
+#  edge=1
+#  total=edge*edge
+#  while total<inp:
+    #  edge=edge+2
+    #  total=edge*edge
+# The block above simply assigns to `total` the smallest square of an odd number
+# >= inp in a very inefficient way.
+
+root = sqrt(inp) # this is a float
+root = int(root) if root == int(root) else int(root) + 1
+edge = root + 1 if root % 2 == 0 else root
+total = edge ** 2 # instead of edge * edge
+
+#  step=(inp-((edge-2)*(edge-2)))%(edge-1)
+step = (inp - (edge-2) ** 2) % (edge - 1)
+#  step=abs(step-((edge-1)/2))
+step = abs(step - (edge - 1) // 2) # // doesn't turn things into floats.
+#  step=step+(edge-1)/2
+step += (edge - 1) // 2 # += is a thing.
 print(step)
 
 def retn(x, y):
     e=max(abs(x), abs(y))
-    t=(e-1)*2+1
-    t=t*t
+    t=((e - 1) * 2 + 1) ** 2
+    #  t=t*t
     if(y==-e):
         return t+(6*e)+(x+e)
     if(x==e):
